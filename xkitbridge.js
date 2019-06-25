af8eb5ac84306c15f15c3a2d7034e20fe6c42875
ndiayepj <52042454+ndiayepj@users.noreply.github.com>
2019-06-25 15:28:06 +0000

    Update xkitbridge.js

--- xkitbridge.js
+++ xkitbridge.js
@@ -4,6 +4,11 @@
 //
 //////
 
-
+var port = Number(process.env.PORT || 9905);
+var iotHost = process.env.IOTHOST || "<MyIoTServer>";
+var iotPort = process.env.IOTPORT || "<MyIoTPort>";
+var IoTuser = process.env.IOTUSER || "<MyIoTUsername>";
+var IoTpass = process.env.IOTPASS || "<MyIoTPassword>";
+var IoTdevicePassword = process.env.IOTDEVPASS || "<MyDefaultDevicePassword>";
 console.log("Setting up IoT Server to");
 
